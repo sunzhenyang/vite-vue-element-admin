f23d6538182ecdeb5ff38181f5ef5dd0dd617946
sunzhenyang <874530670@qq.com>
2022-11-03 09:46:47 +0800

    feat: 集成ElementPlus, 配置按需自动导入对应组件

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterView } from 'vue-router'
   <h1>Vue Test!</h1>
   <svg-icon name="logo"></svg-icon>
   <i class="iconfont icon-Vue" style="font-size: 200px"></i>
+  <el-button type="primary">测试按钮</el-button>
   <RouterView />
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,6 +19,12 @@ export default defineConfig({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
       // 指定symbolId格式
       symbolId: 'icon-[name]'
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
     })
   ],
   resolve: {
