f8e2fe81b4c0f9c8eb2c7492f287a8537c723f19
sunzhenyang <874530670@qq.com>
2022-11-08 17:15:22 +0800

    feat: 生成动态 menu 菜单

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -7,28 +7,11 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu 菜单 -->
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon><i-ep-location /></el-icon>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-sub-menu>
-    <el-sub-menu index="2">
-      <template #title>
-        <el-icon><i-ep-location /></el-icon>
-        <span>导航二</span>
-      </template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-      <el-menu-item index="2-2">选项2</el-menu-item>
-    </el-sub-menu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <el-icon><i-ep-setting /></el-icon>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
@@ -36,13 +19,13 @@
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
+import SidebarItem from './SidebarItem.vue'
 
 const router = useRouter()
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
 })
-console.log(routes.value)
 </script>
 
 <style lang="scss" scoped></style>
