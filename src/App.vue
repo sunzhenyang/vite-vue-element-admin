b46f3ff9a3b95f2409fb71694ec5ba4c5713d54e
sunzhenyang <874530670@qq.com>
2022-11-18 17:25:10 +0800

    feat: element-plus 国际化处理

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <!-- <h1>Vue Test!</h1>
-  <svg-icon name="logo"></svg-icon>
-  <i class="iconfont icon-Vue" style="font-size: 200px"></i>
-  <el-button type="primary">测试按钮</el-button> -->
-  <RouterView />
+  <el-config-provider :locale="language === 'en' ? en : zhCn">
+    <RouterView />
+  </el-config-provider>
 </template>
+
+<script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import en from 'element-plus/dist/locale/en.mjs'
+import { RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { appStore } from '@/stores/app'
+import { LANG } from '@/constant'
+const language = storeToRefs(appStore())[LANG]
+</script>
 
 <style scoped></style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,9 +1,34 @@
 <template>
   <div class="">个人中心</div>
+  <div>
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+const currentPage4 = ref(4)
+const pageSize4 = ref(100)
+const disabled = ref(false)
+const small = ref(false)
+const background = ref(false)
+const handleSizeChange = val => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = val => {
+  console.log(`current page: ${val}`)
+}
 </script>
 
 <style lang="scss" scoped></style>
